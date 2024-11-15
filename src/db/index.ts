b89d95b35dbf97df490196dfb1be2ff251e513c4
Adam Maritz <14345584+Kingfapa@users.noreply.github.com>
2024-11-15 23:23:13 +0100

    added error handling and removed seeding

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,38 +1,8 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { eq } from "drizzle-orm";
-import { usersTable } from "./schema/users";
 
-const db = drizzle(process.env.DATABASE_URL!);
-
-async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: "John",
-    age: 30,
-    email: "john@example.com",
-  };
-  await db.insert(usersTable).values(user);
-  console.log("New user created!");
-  const users = await db.select().from(usersTable);
-  console.log("Getting all users from the database: ", users);
-  /*
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  */
-  await db
-    .update(usersTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log("User info updated!");
-  // await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  // console.log("User deleted!");
+if (process.env.DATABASE_URL === undefined) {
+  throw new Error("DATABASE_URL is not defined in the environment variables");
 }
-main();
 
-export { db };
+export const db = drizzle(process.env.DATABASE_URL);
