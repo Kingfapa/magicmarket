4e1e0ff59fc1109e99664922e1dd14b28ee04302
Adam Maritz <14345584+Kingfapa@users.noreply.github.com>
2024-11-16 10:31:06 +0100

    basic data-table

---
+++ src/app/inventory/columns.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { inventory } from "@/db/schema";
+import { ColumnDef } from "@tanstack/react-table";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+
+export const columns: ColumnDef<typeof inventory.$inferSelect>[] = [
+  {
+    accessorKey: "card.name",
+    header: "Name",
+  },
+  {
+    accessorKey: "price",
+    header: "price",
+  },
+  {
+    accessorKey: "quantity",
+    header: "Quantity",
+  },
+  {
+    accessorKey: "quality",
+    header: "Quality",
+  },
+  {
+    accessorKey: "foil",
+    header: "Foil",
+  },
+];
